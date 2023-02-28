fade0bed1233b2de0b3f490453f00fbf26727918
Matt <matthaynes412@gmail.com>
2023-02-27 19:01:27 -0500

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Card from './components/Card';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>BookIt</h1>
+      <h2>The #1 Community Board for Personal Growth Books!</h2>
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = () => {
+
+    return (
+        <div className="Card">testing the card component</div>
+
+    )
+
+}
+
+export default Card;
