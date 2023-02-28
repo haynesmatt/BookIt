f850a8468dd480e67eaa8628f6b930b3bb4b68ad
Matt <matthaynes412@gmail.com>
2023-02-27 22:28:46 -0500

    Finish Card Display & Styling

---
+++ src/containers/Container.jsx
@@ -0,0 +1,25 @@
+import './Container.css';
+import Card from '../components/Card';
+import React from "react";
+
+const Container = () => {
+
+    return (
+        <div className="Container">
+            <Card name="Atomic Habits" author="James Clear" image="./src/assets/atomic-habits.webp" website="Buy Now"/>
+            <Card name="Deep Work" author="Cal Newport" image="./src/assets/deep-work.png" website="Buy Now"/>
+            <Card name="How to Win Friends & Influence People" author="Dale Carnegie" image="./src/assets/how-to-win-friends.webp" website="Buy Now"/>
+            <Card name="Thinking Fast & Slow" author="Daniel Kahneman" image="./src/assets/thinking-fast-slow.webp" website="Buy Now"/>
+            <Card name="The Subtle Art of Not Giving a F*ck" author="Mark Manson" image="./src/assets/the-subtle-art-of.webp" website="Buy Now"/>
+            <Card name="The 7 Habits of Highly Effective People" author="Stephen R. Covey" image="./src/assets/the-7-habits.webp" website="Buy Now"/>
+            <Card name="The Art of Saying No" author="Damon Zahariades" image="./src/assets/the-art-of-saying-no.webp" website="Buy Now"/>
+            <Card name="Build" author="Tony Fadell" image="./src/assets/build.webp" website="Buy Now"/>
+            <Card name="The Defining Decade" author="Meg Jay" image="./src/assets/the-defining-decade.webp" website="Buy Now"/>
+            <Card name="Think and Grow Rich" author="Napoleon Hill" image="./src/assets/think-and-grow-rich.webp" website="Buy Now"/>
+        </div>
+
+    )
+
+}
+
+export default Container;
